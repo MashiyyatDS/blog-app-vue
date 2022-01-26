d8219dc5d493d60d68ec8c82633cba8d9074eee1
Mashiyyat <delossantos.mashiyyat@gmail.com>
2022-01-26 16:11:44 +0800

    Blog UI Changed

--- src/components/Partials/BlogItem.vue
+++ src/components/Partials/BlogItem.vue
@@ -1,37 +1,33 @@
 <template>
-  <div class="col-md-12 mb-4" v-for="blog in blogs" :key="blog.slug">
-    <div class="card">
-      <div class="card-body">
-        <div class="d-flex justify-content-center bg-dark">
-          <h5 class="card-title p-2">
-            <router-link :to="'/blogs/' + blog.slug" class="text-white">
-              <i class="fa fa-chevron-right"></i> {{ blog.title }}
-            </router-link>
-          </h5>
-        </div>
-        <div class="blog-image-container">
-          <img :src="blog.image" alt="">
-        </div>
-        <div class="blog-tags-container">
-          <button class="btn btn-sm btn-outline-dark disabled m-1" v-for="tag in blog.tags" :key="tag.slug">
-            {{ tag.tag }}
-          </button>
-        </div>
-        <div class="blog-details-container mb-3">
-          <small class="text-muted fw-bolder m-1">Author: {{ blog.user.firstname }} {{ blog.user.lastname }}</small><br>
-          <small class="text-muted m-1">Posted at {{ blog.created_at }}</small>
-        </div>
-        <div class="row">
-          <div class="col-12 p-1">
-            <div class="d-grid">
-              <router-link :to="'/blogs/' + blog.slug" class="btn btn-sm btn-primary">
-                View blog 
-              </router-link>
+  <div class="col-md-10 offset-md-1 mb-3 blog" v-for="blog in blogs" :key="blog.slug">
+    <router-link :to="'/blogs/' + blog.slug">
+      <div class="card">
+        <div class="card-body">
+          <div class="blog-header d-flex p-3">
+            <div class="user-image-container">
+              <img src="@/assets/images/avatar.jpg" alt="" class="user-image">
             </div>
+            <div class="blog-user-name">
+              <p class="card-text fw-bolder ms-2 user-name">{{ blog.user.firstname }} {{ blog.user.lastname }}</p>
+              <small class="ms-2 text-muted blog-date">{{ blog.created_at }}</small>
+            </div>
+          </div>
+          <div class="blog-body">
+            <div class="blog-title-container p-3">
+              <p class="blog-title">{{ blog.title }}</p>
+            </div>
+            <div class="blog-image-container">
+              <img :src="blog.image" alt="" class="blog-image">
+            </div>
+          </div>
+          <div class="blog-footer p-2">
+            <button class="btn btn-sm btn-outline-dark disabled m-1" v-for="tag in blog.tags" :key="tag.id">
+              {{ tag.tag }}
+            </button>
           </div>
         </div>
       </div>
-    </div>
+    </router-link>
   </div>
 </template>
 
@@ -44,10 +40,35 @@ export default {
 
 
 <style scoped>
+  .blog a {
+    text-decoration: none;
+  }
   .card-body {
     padding: 0px;
   }
-  .card-title a {
-    text-decoration: none;
+  .blog-header .user-image {
+    width: 100%;
+  }
+  .user-image-container {
+    width: 50px;
+    height: 50px;
+    border-radius: 30px;
+    overflow: hidden;
+  }
+  .user-name {
+    color: rgb(31, 31, 31);
+    font-family: 'Lexend';
+    margin-bottom: 0px;
+  }
+  .blog-title {
+    margin: 0px!important;
+    font-family: 'Poppins';
+    color: black;
+  }
+  .blog-data {
+    font-size: 3px;
+  }
+  .blog-image-container {
+    width: 100%;
   }
 </style>
