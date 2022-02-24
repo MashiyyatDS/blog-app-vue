5718c8b5a20de3bbc9c1442c8be2277aeef496b2
Mashiyyat <delossantos.mashiyyat@gmail.com>
2022-02-24 11:18:52 +0800

    Axios fixed

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,4 @@ import axios from 'axios'
 
 // axios.defaults.baseURL = 'http://localhost:8000/'
 axios.defaults.baseURL = 'https://mm-blog-app.herokuapp.com/'
-<<<<<<< HEAD
 axios.defaults.headers['Authorization'] = "Bearer " + localStorage.getItem('token')
-=======
-axios.defaults.headers['Authorization'] = "Bearer " + localStorage.getItem('token')
->>>>>>> 39914572a3f3b2d68a48ff3a9575fb622a87b166
