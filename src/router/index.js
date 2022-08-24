f872c0f1e92a519c3704977de5b0b90429888593
Mashiyyat <delossantos.mashiyyat@gmail.com>
2022-08-24 10:41:56 +0800

    undergo maintenance

--- src/router/index.js
+++ src/router/index.js
@@ -1,116 +1,134 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Artworks from '@/views/Pages/Artworks'
-import Blogs from '@/views/Pages/Blogs'
-import Projects from '@/views/Pages/Projects'
-import ViewBlog from '@/views/Pages/ViewBlog'
-import ViewArtwork from '@/views/Pages/ViewArtwork'
-import ViewProject from '@/views/Pages/ViewProject'
-import Login from '@/views/Pages/Login'
-import Dashboard from '@/views/Admin/Dashboard'
-import AccountSettings from '@/views/Admin/AccountSettings'
-import AdminBlogs from '@/views/Admin/Blogs'
-import AdminProjects from '@/views/Admin/Projects'
-import AddBlog from '@/views/Admin/AddBlog'
-import AddProject from '@/views/Admin/AddProject'
-import NotFound from '@/views/Pages/NotFound'
+// import Artworks from '@/views/Pages/Artworks'
+// import Blogs from '@/views/Pages/Blogs'
+// import Projects from '@/views/Pages/Projects'
+// import ViewBlog from '@/views/Pages/ViewBlog'
+// import ViewArtwork from '@/views/Pages/ViewArtwork'
+// import ViewProject from '@/views/Pages/ViewProject'
+// import Login from '@/views/Pages/Login'
+// import Dashboard from '@/views/Admin/Dashboard'
+// import AccountSettings from '@/views/Admin/AccountSettings'
+// import AdminBlogs from '@/views/Admin/Blogs'
+// import AdminProjects from '@/views/Admin/Projects'
+// import AddBlog from '@/views/Admin/AddBlog'
+// import AddProject from '@/views/Admin/AddProject'
+// import NotFound from '@/views/Pages/NotFound'
+import Maintenance from '@/views/Pages/Maintenance'
 import store from '@/store'
+
+// const routes = [
+//   {
+//     path: '/',
+//     name: 'Maintenance',
+//     component: Maintenance
+//   },
+//   {
+//     path: '/*',
+//     name: 'Maintenance',
+//     component: Maintenance
+//   },
+//   // {
+//   //   path: '/',
+//   //   name: 'Blogs',
+//   //   component: Blogs
+//   // },
+//   {
+//     path: '/blogs/:slug',
+//     name: 'ViewBlog',
+//     component: ViewBlog
+//   },
+//   {
+//     path: '/artworks',
+//     name: 'Artworks',
+//     component: Artworks
+//   },
+//   {
+//     path: '/artworks/:slug',
+//     name: 'ViewArtwork',
+//     component: ViewArtwork
+//   },
+//   {
+//     path: '/projects',
+//     name: 'Projects',
+//     component: Projects
+//   },
+//   {
+//     path: '/projects/:slug',
+//     name: 'ViewProject',
+//     component: ViewProject
+//   },
+//   {
+//     path: '/login',
+//     name: 'Login',
+//     component: Login,
+//     meta: {
+//       guest: true
+//     }
+//   },
+//   {
+//     path: '/admin/dashboard',
+//     name: 'Dashboard',
+//     component: Dashboard,
+//     meta: {
+//       auth: true
+//     }
+//   },
+//   {
+//     path: '/admin/account-settings',
+//     name: 'AccountSettings',
+//     component: AccountSettings,
+//     meta: {
+//       auth: true
+//     }
+//   },
+//   {
+//     path: '/admin/blogs',
+//     name: 'AdminBlogs',
+//     component: AdminBlogs,
+//     meta: {
+//       auth: true
+//     }
+//   },
+//   {
+//     path: '/admin/projects',
+//     name: 'AdminProjects',
+//     component: AdminProjects,
+//     meta: {
+//       auth: true
+//     }
+//   },
+//   {
+//     path: '/admin/add-blog',
+//     name: 'AddBlog',
+//     component: AddBlog,
+//     meta: {
+//       auth: true
+//     }
+//   },
+//   {
+//     path: '/admin/add-project',
+//     name: 'AddProject',
+//     component: AddProject,
+//     meta: {
+//       auth: true
+//     }
+//   },
+//   {
+//     path: '/page-not-found',
+//     name: 'NotFound',
+//     component: NotFound
+//   },
+//   {
+//     path: '/:pathMatch(.*)*',
+//     component: NotFound
+//   }
+// ]
 
 const routes = [
   {
-    path: '/',
-    name: 'Blogs',
-    component: Blogs
-  },
-  {
-    path: '/blogs/:slug',
-    name: 'ViewBlog',
-    component: ViewBlog
-  },
-  {
-    path: '/artworks',
-    name: 'Artworks',
-    component: Artworks
-  },
-  {
-    path: '/artworks/:slug',
-    name: 'ViewArtwork',
-    component: ViewArtwork
-  },
-  {
-    path: '/projects',
-    name: 'Projects',
-    component: Projects
-  },
-  {
-    path: '/projects/:slug',
-    name: 'ViewProject',
-    component: ViewProject
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login,
-    meta: {
-      guest: true
-    }
-  },
-  {
-    path: '/admin/dashboard',
-    name: 'Dashboard',
-    component: Dashboard,
-    meta: {
-      auth: true
-    }
-  },
-  {
-    path: '/admin/account-settings',
-    name: 'AccountSettings',
-    component: AccountSettings,
-    meta: {
-      auth: true
-    }
-  },
-  {
-    path: '/admin/blogs',
-    name: 'AdminBlogs',
-    component: AdminBlogs,
-    meta: {
-      auth: true
-    }
-  },
-  {
-    path: '/admin/projects',
-    name: 'AdminProjects',
-    component: AdminProjects,
-    meta: {
-      auth: true
-    }
-  },
-  {
-    path: '/admin/add-blog',
-    name: 'AddBlog',
-    component: AddBlog,
-    meta: {
-      auth: true
-    }
-  },
-  {
-    path: '/admin/add-project',
-    name: 'AddProject',
-    component: AddProject,
-    meta: {
-      auth: true
-    }
-  },
-  {
-    path: '/page-not-found',
-    name: 'NotFound',
-    component: NotFound
-  },
-  {
     path: '/:pathMatch(.*)*',
-    component: NotFound
-  }
+    component: Maintenance
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/Pages/Maintenance.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="d-flex justify-content-center align-items-center" :style="{ height: '100vh' }">
+            <div class="row">
+                <div class="col-12">
+                    <h3><i class="fa fa-wrench"></i> This website is under maintenance  </h3>
+                    <small>This website will undergo maintenance from August 24 until October 27, thanks for patience!</small>
+                </div>
+                <div class="col-12">
+                    <small>~ Kuya Mash 😺</small>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Maintenance'
+}
+</script>
